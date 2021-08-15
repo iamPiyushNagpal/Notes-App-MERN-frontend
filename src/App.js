57653e8a5298ba3e95bcbd2bcf8515e6f60fc570
iamPiyushNagpal <iampiyushnagpal@gmail.com>
2021-08-15 17:48:52 +0530

    Add Login Page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import SignUp from './pages/SignUp/SignUp';
+import LogIn from './pages/LogIn/LogIn';
 
 function App() {
   return (
@@ -9,6 +10,9 @@ function App() {
         <Switch>
           <Route path="/signup">
             <SignUp />
+          </Route>
+          <Route path="/login">
+            <LogIn />
           </Route>
         </Switch>
       </div>
---
+++ src/pages/LogIn/LogIn.js
@@ -0,0 +1,42 @@
+import { useState } from 'react';
+import './LogIn.css';
+import axios from 'axios';
+
+const LogIn = () => {
+
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    const logIn = (e) => {
+        e.preventDefault();
+        axios.post('http://localhost:3001/login', {
+            email,
+            password
+        })
+            .then((res) => console.log(res))
+            .catch((e) => console.log(e.message))
+    }
+
+    return (
+        <div className="log-in">
+            <form onSubmit={logIn}>
+                <h1>Log In</h1>
+                <label>Email</label>
+                <input
+                    type="text"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                />
+                <label>Password</label>
+                <input
+                    type="text"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                />
+                <button>Log In</button>
+            </form>
+        </div>
+    );
+}
+
+export default LogIn;
