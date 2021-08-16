a1cc5b1ce7c6b9d0b4f0def6b9976fda7be13663
iamPiyushNagpal <iampiyushnagpal@gmail.com>
2021-08-16 12:36:29 +0530

    Rename Note.css -> Notes.css

--- src/components/Notes/Notes.js
+++ src/components/Notes/Notes.js
@@ -1,7 +1,7 @@
 import { useEffect, useState } from 'react';
 import axios from 'axios';
 import Masonry, { ResponsiveMasonry } from "react-responsive-masonry"
-import './Note.css';
+import './Notes.css';
 
 const Notes = () => {
 
