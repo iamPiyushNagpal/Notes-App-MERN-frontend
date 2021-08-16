80c03c9691b0ea789c76de3af251891190354ffb
iamPiyushNagpal <iampiyushnagpal@gmail.com>
2021-08-16 13:56:22 +0530

    Check if user is logged in or not

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,7 +1,19 @@
+import { useEffect } from "react";
 import Navbar from "../../components/Navbar/Navbar";
 import Notes from "../../components/Notes/Notes"
+import { useHistory } from "react-router-dom";
 
 const Home = () => {
+
+    const history = useHistory();
+
+    useEffect(() => {
+        const token = localStorage.getItem("userToken");
+        console.log(token);
+        if (!token)
+            history.push('/login');
+    }, []);
+
     return (
         <div className="home">
             <Navbar />
