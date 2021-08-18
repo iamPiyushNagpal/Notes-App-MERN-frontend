990c55e40257669d3ffed6cb78f4f316188449a2
iamPiyushNagpal <iampiyushnagpal@gmail.com>
2021-08-18 12:11:42 +0530

    Add Modal for viewing note in detail

---
+++ src/components/Modal/Modal.js
@@ -0,0 +1,27 @@
+import './Modal.css';
+
+const Modal = ({ title, description, setShowModal }) => {
+
+    const handleCloseButton = () => {
+        setShowModal(false);
+    }
+
+    return (
+        <div className="modal">
+            <div className="modal__content">
+                <h1>{title}</h1>
+                <p>{description}</p>
+            </div>
+            <div className="modal__buttons">
+                <button><svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+                    <path fill-rule="evenodd" d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z" clip-rule="evenodd" />
+                </svg></button>
+                <button onClick={handleCloseButton}><svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+                    <path fillRule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clipRule="evenodd" />
+                </svg></button>
+            </div>
+        </div>
+    );
+}
+
+export default Modal;
--- src/components/Notes/Notes.js
+++ src/components/Notes/Notes.js
@@ -2,10 +2,14 @@ import { useEffect, useState } from 'react';
 import axios from 'axios';
 import Masonry, { ResponsiveMasonry } from "react-responsive-masonry"
 import './Notes.css';
+import Modal from '../Modal/Modal';
 
 const Notes = () => {
 
     const [data, setData] = useState([]);
+    const [showModal, setShowModal] = useState(false);
+    const [description, setDescription] = useState("");
+    const [title, setTitle] = useState("");
 
     useEffect(() => {
         axios.get('http://localhost:3001/get-notes')
@@ -16,18 +20,25 @@ const Notes = () => {
             .catch((e) => console.log(e.message))
     }, []);
 
+    const handleModal = (title, description) => {
+        setShowModal(true);
+        setTitle(title);
+        setDescription(description);
+    }
+
     return (
         <div className="notes">
             <ResponsiveMasonry columnsCountBreakPoints={{ 350: 2, 750: 3, 900: 4, 1200: 5 }}>
                 <Masonry gutter={"20px"}>
                     {data && data.map((note) => (
-                        <div className="note" key={note._id}>
+                        <div className="note" key={note._id} onClick={() => handleModal(note.title, note.description)}>
                             <h1>{note.title}</h1>
                             <p>{note.description.slice(0, 70)}</p>
                         </div>
                     ))}
                 </Masonry>
             </ResponsiveMasonry>
+            {showModal && <Modal setShowModal={setShowModal} title={title} description={description} />}
         </div>
     );
 }
